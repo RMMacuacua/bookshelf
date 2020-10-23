eef7e235dd8a3e99047a351303307b308d113b63
RMMacuacua <40232102+RMMacuacua@users.noreply.github.com>
2020-10-23 09:34:18 +0200

    criada a TABELA books

---
+++ database/migrations/2020_10_22_220715_create_table_books.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableBooks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('editora');
+            $table->string('edicao');
+            $table->double('preco',8,3);
+            $table->string('isbn');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
